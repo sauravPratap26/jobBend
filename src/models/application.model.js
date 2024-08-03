2656f27bf06316464d4781f1a4b19c390b01a5db
Saurav <sauravcloud26@gmail.com>
2024-08-03 15:11:41 +0530

    application, company, job, user models created

---
+++ src/models/application.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+    },
+    applicant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "ACCEPTED", "REJECTED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Application = mongoose.model("Application", applicationSchema);
---
+++ src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["STUDENT", "RECRUIT"],
+    },
+    profile: {
+      bio: { type: String },
+      skills: [{ type: String }],
+      resume: { type: String },
+      resumeOriginalName: { type: String },
+      company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+      },
+      profilePhoto: {
+        type: String,
+        default: "",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
