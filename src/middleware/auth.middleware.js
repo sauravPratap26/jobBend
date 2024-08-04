886f096d6237de8b2dfc726008870ebc0e6be747
Saurav <sauravcloud26@gmail.com>
2024-08-04 10:45:31 +0530

    auth middleware written

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import ApiError from "../utils/ApiError";
+
+export const verifyJwt = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+  } catch (error) {
+    return res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
