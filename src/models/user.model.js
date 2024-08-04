1309dfe719ca7fec48fb7d85d798848c8a38fce9
Saurav <sauravcloud26@gmail.com>
2024-08-04 10:07:03 +0530

    small changes in the user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    fullName: {
+    email: {
       type: String,
       required: true,
       unique: true,
@@ -37,6 +37,9 @@ const userSchema = new mongoose.Schema(
         type: String,
         default: "",
       },
+      refreshToken: {
+        type: String,
+      },
     },
   },
   { timestamps: true }
@@ -52,4 +55,32 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.isRoleCorrect = async function (role) {
+  return role === this.role;
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this._username,
+      roles: this.roles
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", userSchema);
