d692c8c331d84ea15161dfa42a026cdcfd2e221d
Saurav <sauravcloud26@gmail.com>
2024-12-29 10:41:10 +0530

    added multer code file and basic changes in the cors origin

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRouter from "./routes/user.routes.js"
-import companyRouter from "./routes/company.routes.js"
-import jobRouter from "./routes/job.routes.js"
-import applicationRouter from "./routes/application.routes.js"
+import userRouter from "./routes/user.routes.js";
+import companyRouter from "./routes/company.routes.js";
+import jobRouter from "./routes/job.routes.js";
+import applicationRouter from "./routes/application.routes.js";
+
+dotenv.config(); // Load .env variables
+
 const app = express();
+
+const allowedOrigins = process.env.CORS_ORIGIN?.split(","); // Support multiple origins
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow if the origin is in the allowed list
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
@@ -19,9 +31,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/company",companyRouter)
-app.use("/api/v1/job",jobRouter)
-app.use("/api/v1/application",applicationRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/company", companyRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
 
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,8 +6,9 @@ import {
   updateProfile,
 } from "../controller/user.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
+import { singleUpload } from "../middleware/multer.js";
 const router = Router();
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 
 //secured routes
